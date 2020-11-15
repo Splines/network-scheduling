4a3f5d80dcfb4b5e9c3211c8b8278234cea7643a
Ordinateur-Hack <dominic-plein@gmx.de>
2020-11-15 19:36:41 +0100

    Improve graphical representation of waiting time

--- src/index.py
+++ src/index.py
@@ -2,7 +2,7 @@ from copy import deepcopy
 
 import plot
 from package_simulation import simulate_packages
-from round_robin import round_robin
+from scheduling import round_robin, fair_queuing
 
 print('# =============== #')
 print('# Scheduling Plot #')
@@ -10,20 +10,23 @@ print('# =============== #')
 print('© Dominic Plein 11/2020')
 
 # Plot
-ax1, ax2, ax3 = plot.init_plot()
+fig, (ax1, ax2, ax3, ax4, ax5) = plot.init_plot()
 
 # Packages
-sources, max_end_time = simulate_packages(10, 50, 10)
+sources, max_end_time = simulate_packages(15, 40, 10)
 plot.plot_broken_barh(ax1, 'Eintreffende Pakete', sources, max_end_time)
 
 # Round Robin (abbreviated as rr)
 sources_rr, diff_rr, max_end_time_rr, data_rr = round_robin(deepcopy(sources))
-ax1.set_xlim(0,max_end_time_rr) # TODO: outsource to plot
 plot.plot_broken_barh(ax2, 'Round-Robin', sources_rr, max_end_time_rr, diff_rr)
-plot.plot_scatter(ax3, 'Round-Robin (Auswertung)', data_rr)
+plot.plot_scatter(ax4, 'Round-Robin (Auswertung)', data_rr)
 
-# Fair Queuing
-# plot_scatter(ax3, data_rr)
+# Fair Queuing (abbreviated as fq)
+sources_fq, diff_fq, max_end_time_fq, data_fq = fair_queuing(deepcopy(sources))
+plot.plot_broken_barh(ax3, 'Fair Queuing', sources_fq, max_end_time_fq, diff_fq)
+plot.plot_scatter(ax5, 'Fair Queuing (Auswertung)', data_fq)
 
 # Plot
+ax1.set_xlim(0, max(max_end_time_rr, max_end_time_fq))
+plot.save(fig)
 plot.show_plot()
--- src/plot.py
+++ src/plot.py
@@ -1,14 +1,25 @@
+import os
+
 import matplotlib.pyplot as plt
 
 
 def init_plot():
-    fig, (ax1, ax2, ax3) = plt.subplots(3, 1)
+    fig, (ax1, ax2, ax3, ax4, ax5) = plt.subplots(5, 1)
     fig.suptitle('Scheduling-Verfahren', fontsize=16)
-    return ax1, ax2, ax3
+    return fig, (ax1, ax2, ax3, ax4, ax5)
 
 
 def show_plot():
     plt.show()
+
+
+def plot_waiting_time(big_height, y_tick, diff, axes, face_color, edge_color):
+    delta = big_height / len(diff)
+    i = 0
+    for element in diff:
+        if element[1] != 0:
+            axes.broken_barh([element], (y_tick + big_height / 2 - delta * i - 1.5, 1.5), fc=face_color, ec=edge_color)
+        i += 1
 
 
 def plot_broken_barh(axes, title, sources, max_end_time, diff=None):
@@ -17,13 +28,14 @@ def plot_broken_barh(axes, title, sources, max_end_time, diff=None):
     axes.set_ylim(5, 45)
     axes.set_xlim(0, max_end_time)
 
-    axes.broken_barh(sources[0], (31, 8), fc='#F0E156', ec='black')
-    axes.broken_barh(sources[1], (21, 8), fc='#F56670', ec='black')
-    axes.broken_barh(sources[2], (11, 8), fc='#16BFE0', ec='black')
+    big_height = 8
+    axes.broken_barh(sources[0], (35 - big_height / 2, big_height), fc='#F0E156', ec='black')
+    axes.broken_barh(sources[1], (25 - big_height / 2, big_height), fc='#F56670', ec='black')
+    axes.broken_barh(sources[2], (15 - big_height / 2, big_height), fc='#16BFE0', ec='black')
     if diff:
-        axes.broken_barh(diff[0], (34, 2), fc='#F2EAA0')
-        axes.broken_barh(diff[1], (24, 2), fc='#F7B1B6')
-        axes.broken_barh(diff[2], (14, 2), fc='#8DE1F2')
+        plot_waiting_time(big_height, 35, diff[0], axes, '#F2EAA0', '#D1C97D')
+        plot_waiting_time(big_height, 25, diff[1], axes, '#F7B1B6', '#DB8489')
+        plot_waiting_time(big_height, 15, diff[2], axes, '#8DE1F2', '#77C8D9')
     axes.set_yticks([15, 25, 35])
     axes.set_yticklabels(['Quelle 3', 'Quelle 2', 'Quelle 1'])
 
@@ -34,3 +46,12 @@ def plot_scatter(axes, title, data):
     axes.set_ylabel('Wartezeit')
     x, y = zip(*data)
     axes.scatter(x, y)
+
+
+def save(fig):
+    # plt.tight_layout()
+    plt.subplots_adjust(top=0.9, hspace=0.7)
+    fig.set_size_inches(12, 13, forward=True)
+    fname = os.path.join(os.environ["HOMEPATH"], "Desktop", "Scheduling.svg")
+    fig.savefig(fname, transparent=False)
+    print('Saved file to: ', fname)
